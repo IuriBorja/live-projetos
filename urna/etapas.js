43e114832db4d078e97e9bdf679d039f394f4a96
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-07-14 01:05:08 +0100

    att 1

---
+++ urna/etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+        {
+            numero: '55555',
+            nome: 'Dexter',
+            partido: 'Laboratório',
+            fotos: [
+                {url: 'dexter.png', legenda: 'Vereador'}
+            ]
+        },
+        {
+            numero: '77777',
+            nome: 'Johnny Bravo',
+            partido: 'E ai, gatinha?',
+            fotos: [
+                {url: 'Johnny-Bravo.webp', legenda: 'Vereador'}
+            ]
+        },
+        ]
+    },
+    {
+
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Du',
+                partido: 'Plunk',
+                vice: 'Dexter',
+                fotos: [
+                    {url: 'du.jpg', legenda: 'Prefeito'},
+                    {url: 'dexter.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Vaca e o Frango',
+                partido: 'Mamãe e Papai',
+                vice: 'Du',
+                fotos:[
+                    {url: 'vaca-e-frango.jpg', legenda: 'Prefeito'},
+                    {url: 'du.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+
+    }
+];
